600be80b2d70fffb39a6081bb9e533f3e5c343eb
Daniel.Gomez <dgomez.informatica@gmail.com>
2021-03-22 17:30:54 -0300

    se soluciono el problema del ajax, ahora si me redirije bien

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -1,21 +1,21 @@
 //CODE AUTENTIFICACION
-//$("#btnEnviar").click(function () {
-//    let usuario = $('#txtUsuario').val();
-//    let password = $('#txtClave').val();
-//
-//    if (usuario.length <= 0) {
-//        alert("Debe ingersra un usuario");
-//        return false;
-//    } 
-//    
-//    if (password.length <= 0) {
-//        alert("Debe ingresar una clave");
-//        return false;
-//    }
-//    
-//    $.ajax({
-//        url: "Servlets/Svl_Autentificacion",
-//        type: "POST"
-//        
-//    });
-//});
+$("#btnEnviar").click(function () {
+    let usuario = $('#txtUsuario').val();
+    let password = $('#txtClave').val();
+
+    if (usuario.length <= 0) {
+        alert("Debe ingersra un usuario");
+        return false;
+    } 
+    
+    if (password.length <= 0) {
+        alert("Debe ingresar una clave");
+        return false;
+    }
+    
+    $.ajax({
+        url: "Servlets/Svl_Autentificacion",
+        type: "POST"
+        
+    });
+});
