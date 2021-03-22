84298e8078c145751f7a3b7e51bdeed3e8d6a4aa
Daniel.Gomez <dgomez.informatica@gmail.com>
2021-03-22 19:58:44 -0300

    se crea codigo provisorio para enviar los datos al servlert

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -1,21 +1,34 @@
 //CODE AUTENTIFICACION
 $("#btnEnviar").click(function () {
-    let usuario = $('#txtUsuario').val();
-    let password = $('#txtClave').val();
+    let usuario = $('#txtUsuario').val().trim();
+    let clave = $('#txtClave').val().trim();
+    let parametros = new Array();
 
     if (usuario.length <= 0) {
         alert("Debe ingersra un usuario");
         return false;
-    } 
-    
-    if (password.length <= 0) {
+    }
+
+    if (clave.length <= 0) {
         alert("Debe ingresar una clave");
         return false;
     }
-    
+
+    parametros.push(usuario, clave);
     $.ajax({
-        url: "Servlets/Svl_Autentificacion",
-        type: "POST"
-        
+        url: "Svl_Usuario",
+        type: "POST",
+        dataType: "json",
+        data: {
+            accion: "Authenticar",
+            informacion: JSON.stringify(parametros)
+        },
+        success: function (data) {
+            if (data.estado == 200) {
+                console.info(data.datos);
+            } else {
+                console.error("E: Contactar con informatica");
+            }
+        }
     });
 });
